72d3753bdd18001f0a2ffa5cf31e1f86785df4f7
Tina284 <77157761+Tina284@users.noreply.github.com>
2024-04-11 18:23:16 +0200

    Add files via upload

---
+++ 19_predavanje/petlje.js
@@ -0,0 +1,40 @@
+/*for (let i = 0; i < 5; i++){
+    console.log(i);
+};
+console.log(i);*/
+/*
+let broj = 4
+while (broj <5){
+    console.log("while", broj);
+    broj++;
+}
+let broj2 = 4
+do {
+    console.log("do while", broj2);
+    broj2++;
+} while (broj2 < 5)
+*/
+//Zd 1. 
+/*for (let i = 2; i <= 20; i += 2){
+    console.log(i);
+}*/
+
+//Zd 2.
+/*
+let zbroj = 0
+for (let i = 1; i <= 5; i++){
+    zbroj += i;
+}
+console.log(zbroj);*/
+
+//Zd 3.
+/*const niz = ["HTML", "CSS", "JS"];
+for (let i = 0; i<niz.length; i++){
+    console.log(niz[i])
+}*/
+
+//Zd 4.
+let rijec = "programiranje";
+for (let i = 0; i < rijec.length; i++){
+    console.log(rijec[i]);
+}
---
+++ 19_predavanje/zadatak.js
@@ -0,0 +1,8 @@
+var broj = 31;
+
+if (broj % 3 == 0 && broj % 5 == 0) {
+    console.log("broj je djeljiv s 3 i 5.")
+}
+else {
+    console.log("Broj nije djeljiv s 3 i 5.")
+}
